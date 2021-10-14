04c89b85b2e371ac17c1a55722248e1ef87ee6ea
Nhom30 <nguyenduythuong2k@gmail.com>
2021-10-14 19:50:58 +0700

    initial

---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+
+const express =require('express')
+const mongoose =require('mongoose')
+const cors = require('cors')
+const fileUpload =require('express-fileupload')
+const cookieParser =require('cookie-parser')
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}));
+
+//Routers
+app.use('/user',require('./routers/userRouter'))
+//connect to db mongo
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI,{
+    //mongoose 6.0.6 ko hoox trojw cacs hamf duois
+    // userCreateIndex: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},err=>{
+    if(err) throw err;
+    console.log('Connected to MongoDB')
+})
+
+const PORT = process.env.PORT||8000
+
+// app.get('/',(req,res)=>{
+//     res.json({msg:"WELCOME"})
+// })
+
+app.listen(PORT,()=>{
+    console.log('Server is running on Port',PORT)
+})
